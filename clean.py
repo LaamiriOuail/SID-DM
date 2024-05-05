2fed7ba4641163a239afa92d0cd1f8e16fa51df1
Ouail Laamiri <laamiri.ouail@etu.uae.ac.ma>
2024-05-05 20:39:40 +0100

    Task 1 :The result etudiant data is cleaning and handle the missign value

--- clean.py
+++ clean.py
@@ -15,10 +15,49 @@ def clean_note_value(note):
                 return note / power
     return note
 
+# Function to set the value of the RESULT column based on the NOTE column
+def set_result(note):
+    if note >= 10:
+        return 'V'
+    elif note < 10 and note >= 7:
+        return 'AC'
+    elif note < 7:
+        return 'NV'
+    else:
+        return 'NP'
+
+# Function to clean the note values
+def clean_null_note_value(note):
+    if pd.isnull(note):
+        return 0  # Change null values to 0
+    return note
+
+# Function to fill missing years in the ANNE column with the value from the previous row
+def fill_missing_by_privious(df,column):
+    if column in df.columns:
+        previous_year = None
+        for index, row in df.iterrows():
+            if pd.isnull(row[column]):
+                df.at[index, column] = previous_year
+            else:
+                previous_year = row[column]
+    else:
+        print(f'{column} not a column in dataframe')
+
+
 # Apply the clean_note_value function to the NOTE column
 df['NOTE'] = df['NOTE'].apply(clean_note_value)
 
+# Apply the set_result function to the NOTE column and create the RESULT column
+df['RESULT'] = df['NOTE'].apply(set_result)
+
+# Apply the clean_note_value function to the NOTE column
+df['NOTE'] = df['NOTE'].apply(clean_null_note_value)
+
+fill_missing_by_privious(df,"ANNE")
+fill_missing_by_privious(df,"SESSION")
+
 # Save the cleaned DataFrame back to a CSV file
-df.to_csv(base+"/cleaned_note_result.csv", index=False)
+df.to_csv(base+"/cleaned_module_result.csv", index=False)
 
 print("Data cleaned and saved as cleaned_output.csv")
