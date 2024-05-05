6aacc1db0199e04f5e3aae678a30e260d9b1d87c
Ouail Laamiri <laamiri.ouail@etu.uae.ac.ma>
2024-05-05 20:48:56 +0100

    finish task 1

--- display.py
+++ display.py
@@ -1,6 +1,11 @@
 import pandas as pd 
 
-df = pd.read_csv("DATA/RES/result.csv")
+df = pd.read_csv("DATA/RES/cleaned_module_result.csv")
 
 
-print(df)
+# Get the count of null values for each column
+null_counts = df.isnull().sum()
+
+print("Null value counts for each column:")
+print(null_counts)
+
